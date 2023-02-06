5e2d001994a70e0cca832e2170657776a2b9ac66
Andrew Alley <andrew.alley99@gmail.com>
2023-02-06 13:01:47 -0700

    Add project files.

---
+++ Mission_6_alley725/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission_6_alley725.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
