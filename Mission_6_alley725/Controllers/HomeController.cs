894ef22d132760c060dddb94c5f58867760b155c
Andrew Alley <andrew.alley99@gmail.com>
2023-02-06 22:50:02 -0700

    Database connection

--- Mission_6_alley725/Controllers/HomeController.cs
+++ Mission_6_alley725/Controllers/HomeController.cs
@@ -13,9 +13,13 @@ namespace Mission_6_alley725.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private MovieContext _db;
+
+        public HomeController(ILogger<HomeController> logger, MovieContext movie)
         {
             _logger = logger;
+
+            _db = movie;
         }
 
         public IActionResult Index()
@@ -26,6 +30,35 @@ namespace Mission_6_alley725.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm()
+        {  
+
+            ViewData["Ratings"] = new string[] { "G", "PG", "PG-13", "R" };
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(Movies movie)
+        { 
+            _db.Add(movie);
+            _db.SaveChanges();
+            var data = _db.Movies.ToList();
+            return View("Data", data);
+        }
+
+        public IActionResult Data()
+        {
+            var data = _db.Movies.ToList();
+            return View(data);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Mission_6_alley725/Migrations/20230207050051_data.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission_6_alley725.Migrations
+{
+    public partial class data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    movieId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    title = table.Column<string>(nullable: false),
+                    rating = table.Column<string>(nullable: false),
+                    category = table.Column<string>(nullable: false),
+                    year = table.Column<int>(nullable: false),
+                    edited = table.Column<bool>(nullable: false),
+                    lentTo = table.Column<string>(nullable: true),
+                    notes = table.Column<string>(maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.movieId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
